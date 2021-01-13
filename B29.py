2e633aa8e7d4d682c626fcdb4685c410e63401b7
Ionela99 <62048556+Ionela99@users.noreply.github.com>
2021-01-13 23:44:36 +0200

    fixed some bugs

--- B29.py
+++ B29.py
@@ -1,27 +1,38 @@
-#import matplotlib.pyplot as plt
-#from scipy import stats
+import matplotlib.pyplot as plt
+from scipy import stats
 import numpy as np
 
-x = np.array([2, 3, 5, 7, 9])
-y = np.array([4, 5, 7, 10, 15])
+#x =  np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+#y =  np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+
+#x = np.array([2, 3, 5, 7, 9])
+#y = np.array([4, 5, 7, 10, 15])
+
+x = np.array([1.21, 3.00, 5.16, 8.31, 10.21])
+y = np.array([1.69, 5.89, 4.11, 5.49, 8.65])
+
 n = len(x)
 x2 = np.array([])
 x2 = [0 for i in range(n)]
 xy = np.array([])
 xy = [0 for i in range(n)]
-m=0
-b=0
+m=0    #slope
+b=0    #intercept
 sumax = 0
 sumay = 0
 sumax2 = 0
 sumaxy = 0
 
+def f(a):
+    for i in range(n):
+        format(a[i], '.3f')
+
 def power(x):
     j = 0
-
     for i in range(n):
         x2[j] = x[i]**2
         j += 1
+    f(x2)
     print("x^2 este egal cu:", x2)
     #print(x2)
 
@@ -72,10 +83,11 @@ def slope(a, b, c, d):
 
 #slope(sumax, sumay, sumax2, sumaxy)
 
-def Intercept(a, b, c, d):
+def Intercept(a, be, c, d):
     global sumax
     global sumay
     global n
+    global b
     slope(sumax, sumay, sumax2, sumaxy)
 
     b = (sumay - m * sumax) / n
@@ -83,6 +95,16 @@ def Intercept(a, b, c, d):
 
 Intercept(sumax, sumay, m, n)
 
-#plt.scatter(x, y)
-#plt.show()
+def myfunc(x):
+    global b
+    global m
+    return m * x + b
+
+mymodel = list(map(myfunc, x))
+
+print(mymodel)
+
+plt.scatter(x, y)  #punctele in sistem
+plt.plot(x, mymodel)
+plt.show()
 
